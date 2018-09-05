318500427ded64fee5db932dd4304c32bac4816f
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2018-09-04 20:57:24 -0700

    Render start page from a null timer

--- lib.js
+++ lib.js
@@ -4,33 +4,62 @@ module.exports = {
     Timer: Timer,
 }
 
-function Timer(duration, segments, now) {
-    this.duration = duration; // milliseconds
+// todo: figure out how the timer ending works
+function Timer(duration, segments) {
+    this.duration = duration;
     this.segments = segments;
-    this.start = now;
 
-    this.running = true;
-    this.paused = false;
+    this.running = false;
     this.width = 5; // progress bars
 
+    this.start = function(now) {
+	this.running = true;
+	this.startTime = now;
+	return this;
+    }
+
     this.segmentedTime = function(now) {
-
-	let left = this.duration - (now - this.start);
-	let segmentLen = this.duration / this.segments;
-	let div = left / segmentLen | 0;
-	let rem = left % segmentLen;
-
 	let result = [];
-	for (let i = 0; i < segments - div - 1; i++) { result[i] = 0; }
-	result[segments - div - 1] = rem;
-	for (let i = segments - div; i < segments; i++) { result[i] = segmentLen; }
+	let segmentLen = this.duration / this.segments;
+
+	if (this.running) {
+	    let left = this.duration - (now - this.startTime);
+	    let div = left / segmentLen | 0;
+	    let rem = left % segmentLen;
+
+	    for (let i = 0; i < segments - div - 1; i++) { result[i] = 0; }
+	    result[segments - div - 1] = rem;
+	    for (let i = segments - div; i < segments; i++) { result[i] = segmentLen; }
+	} else {
+	    for (let i = 0; i < segments; i++) { result[i] = segmentLen; }
+	}
 
 	result.render = function() {
-	    return result.map(v => `<progress value = "${v}" max = "${segmentLen}"></progress>`).join('\n');
+	    return result.map(v => `<progress value = "${v}" max = "${segmentLen}"></progress>`).join('<br />');
 	};
 
 	return result;
     };
-}
 
-function ProgressVal(width, segments, duration, remaining) {}
+    this.render = function(now) {
+	let left = this.duration;
+	if (this.running) { left = now - this.start; }
+
+	let formatted = new Date(left).toISOString().substr(11, 8).replace(/^[0:]+/, "");
+
+	let result = `<div id = "timer">${formatted}</div>`;
+
+	result += this.segmentedTime(now).render();
+
+	result += `<div><button id = "start" type = "submit" value = "start">Start</button>`;
+	result += `<button id = "advance" type = "submit" value = "advance">Advance</button></div>`;
+
+	result += `<div>`;
+	result += `<label for = "total">Total Time: </label> <input type = "text" id = "total" name = "total">`;
+	result += `<label for = "segments">Segments: </label> <input type = "text" id = "segments" name = "segments">`;
+	result += `<label for = "penalty">Penalty: </label>\n<input type = "text" id = "penalty" name = "penalty">`;
+	result += `</div>`;
+
+	return result;
+    };
+}
--- test.js
+++ test.js
@@ -22,8 +22,9 @@ let d4 = new Date(2000, 0, 1, 0, 1, 10);
 run(
     test(true, true),
 
-    test(new lib.Timer(70000, 7, d1).segmentedTime(d1), [10000, 10000, 10000, 10000, 10000, 10000, 10000]),
-    test(new lib.Timer(70000, 7, d1).segmentedTime(d2), [0, 0, 0, 0, 0, 5000, 10000]),
-    test(new lib.Timer(70000, 7, d1).segmentedTime(d3), [0, 0, 0, 0, 0, 10000, 10000]),
-    test(new lib.Timer(70000, 7, d1).segmentedTime(d4), [0, 0, 0, 0, 0, 0, 0]),
+    test(new lib.Timer(70000, 7).segmentedTime(d4), [10000, 10000, 10000, 10000, 10000, 10000, 10000]),
+    test(new lib.Timer(70000, 7).start(d1).segmentedTime(d1), [10000, 10000, 10000, 10000, 10000, 10000, 10000]),
+    test(new lib.Timer(70000, 7).start(d1).segmentedTime(d2), [0, 0, 0, 0, 0, 5000, 10000]),
+    test(new lib.Timer(70000, 7).start(d1).segmentedTime(d3), [0, 0, 0, 0, 0, 10000, 10000]),
+    test(new lib.Timer(70000, 7).start(d1).segmentedTime(d4), [0, 0, 0, 0, 0, 0, 0]),
 );
